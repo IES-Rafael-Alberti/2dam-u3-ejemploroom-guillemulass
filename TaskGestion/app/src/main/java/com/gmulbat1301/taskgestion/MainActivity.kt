b0685ad8c5fe1cd5cf2a4e03aba8229fc93c41f1
guillemulass <gmulbat1301@g.educaand.es>
2024-01-12 19:36:14 +0100

    Parte 1 terminada

---
+++ TaskGestion/app/src/main/java/com/gmulbat1301/taskgestion/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.gmulbat1301.taskgestion
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.gmulbat1301.taskgestion.addtask.ui.TasksScreen
+import com.gmulbat1301.taskgestion.addtask.ui.TasksViewModel
+import com.gmulbat1301.taskgestion.ui.theme.TaskGestionTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    private val tasksViewModel: TasksViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskGestionTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TasksScreen(tasksViewModel)
+                }
+            }
+        }
+    }
+}
---
+++ TaskGestion/app/src/main/java/com/gmulbat1301/taskgestion/TasksManageApp.kt
@@ -0,0 +1,7 @@
+package com.gmulbat1301.taskgestion
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TasksManageApp: Application()
